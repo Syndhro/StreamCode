b83e36128c2eb00297dab34a86267297e37ace0f
andreavaghi93 <andreavaghi93@yahoo.it>
2015-11-07 15:39:59 +0100

    Easy implementation of DB connection

--- StreamCode/src/com/DBManager.java
+++ StreamCode/src/com/DBManager.java
@@ -1,14 +1,35 @@
 package com;
 
-//YO 
+import java.sql.*;
+
+import com.mysql.jdbc.PreparedStatement;
+import com.mysql.jdbc.Statement;
 
 public class DBManager {
 //db stuff
 	private String dbusername;
 
 	private String dbpassword;
+	
+	public DBManager(){
+		connect();
+	}
 
-	public void connect(){
+	public Connection connect(){
+		Connection connection = null;
+		try {
+		     Class.forName("com.mysql.jdbc.Driver");
+		     
+		     connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/streamdata", "root", "93Matrix");			      	
+	    }
+	    catch(ClassNotFoundException e) {
+	      e.printStackTrace();
+	    }
+	    catch(SQLException e) {
+	      e.printStackTrace();
+	    }
+		
+		return connection;
 	}
 
 	public void addUser() {
@@ -34,4 +55,24 @@ public class DBManager {
 
 	public void removeNotification() {
 	}
+	
+	public static void main(String[] args){
+		
+		DBManager dbmanager = new DBManager();
+		
+		Statement statement = null;
+	    ResultSet resultSet = null;
+		
+	    try {
+		 String query = "SELECT * FROM project";
+	     statement = (Statement) dbmanager.connect().createStatement();           
+	     resultSet = statement.executeQuery(query);
+	     while (resultSet.next())
+				System.out.println(resultSet.getString(1));
+	    }catch(Exception e){
+	    	e.printStackTrace();
+	    }
+		
+	}
+	
 }
