ee95734755877dc6b454b7d902d832626f325718
andreavaghi93 <andreavaghi93@yahoo.it>
2015-11-16 15:12:58 +0100

    Notification class improved

--- StreamCode/src/com/Notification.java
+++ StreamCode/src/com/Notification.java
@@ -6,10 +6,12 @@ import java.util.Date;
 
 public abstract class Notification implements Serializable{
 
+	private int notificationId;
 	private String message;
 	private Time time;
 	private Date date;
 	private int sourceID;
 	private int targetID;
+	private boolean delivered;
 
 }
