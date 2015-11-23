ce9bb1f454b6b9d0e6679c1f6d56f63cf7b4cea5
andreavaghi93 <andreavaghi93@yahoo.it>
2015-11-23 10:27:08 +0100

    Pre pull from aniel

--- StreamCode/src/com/ActivityModifierPanel.java
+++ StreamCode/src/com/ActivityModifierPanel.java
@@ -10,11 +10,8 @@ import java.util.ArrayList;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JCheckBox;
-import javax.swing.JComboBox;
-import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 import javax.swing.SwingUtilities;
 
 public class ActivityModifierPanel extends JPanel {
@@ -51,75 +48,8 @@ public class ActivityModifierPanel extends JPanel {
 			}
 		}
 		add(collaboratorsPanel);
-		
-		JLabel activityName = new JLabel(activity.getName());
-		JLabel activityDescription = new JLabel(activity.getDescription());
-		JLabel activityPlace = new JLabel(activity.getPlace());
-	
-		JPanel activityValues = new JPanel();
-		activityValues.setLayout(new BoxLayout(activityValues, BoxLayout.PAGE_AXIS));
-		activityValues.add(new JLabel("Name: "));
-		activityValues.add(activityName);
-		activityValues.add(new JLabel(" "),"span, grow");
-		activityValues.add(new JLabel("Description: "));
-		activityValues.add(activityDescription);
-		activityValues.add(new JLabel(" "),"span, grow");
-		activityValues.add(new JLabel("Place: ")); //fare combobox
-		activityValues.add(activityPlace);	
-		add(activityValues);
-		
-		//MODIFY INFO ACTIVITY-------------------------------------------------
-		JButton modifyInfo = new JButton("Modify Activity Info");
-		add(modifyInfo);
-		modifyInfo.addActionListener(new ActionListener(){
-
-			@Override
-			public void actionPerformed(ActionEvent e) {
-
-				JTextField activityTitle2 = new JTextField(15);
-				JTextField activityDescription2 = new JTextField(40);
-				JTextField activityPlace2 = new JTextField(15);
-						
-				JPanel activityValues2 = new JPanel();
-				activityValues2.setLayout(new BoxLayout(activityValues2, BoxLayout.PAGE_AXIS));
-				activityValues2.add(new JLabel("Insert title: "));
-				activityValues2.add(activityTitle2);
-				activityValues2.add(new JLabel(" "),"span, grow");
-				activityValues2.add(new JLabel("Insert description: "));
-				activityValues2.add(activityDescription2);
-				activityValues2.add(new JLabel(" "),"span, grow");
-				activityValues2.add(new JLabel("Insert place: ")); //fare combobox
-				activityValues2.add(activityPlace2);
-				activityValues2.setVisible(true);
-				
-				JOptionPane creatingProject = new JOptionPane();
-				int result = creatingProject.showConfirmDialog(thisPanel, activityValues2, "Modify Activity Values", JOptionPane.OK_CANCEL_OPTION);
-				if (result == creatingProject.OK_OPTION){	
-					/*	questa parte va fatta sul server, ci vuole una funzione modify activity
-					Activity activity2 = null;
-						try {
-							activity2 = Client.getInstance().getManagedProject().get(projectIndex).getActivities().get(activityIndex);
-						} catch (RemoteException e1) {
-							// TODO Auto-generated catch block
-							e1.printStackTrace();
-						}
-						activity2.setName(activityTitle2.getText());
-						activity2.setDescription(activityDescription2.getText());
-						activity2.setPlace(activityPlace2.getText());		
-						
-						*/
-						Window w = SwingUtilities.getWindowAncestor(thisPanel);	//codice per nascondere 
-						w.setVisible(false);									//la vecchia finestra		
-						JOptionPane newactivityPane2 = new JOptionPane();
-						ActivityModifierPanel newactivityPanel2 = new ActivityModifierPanel(projectIndex, activityIndex, parentPanel);
-						newactivityPane2.showConfirmDialog(parentPanel, newactivityPanel2,"Activities", JOptionPane.CLOSED_OPTION);
-				}
-				
-			}
-			
-		});
 	    
-	    //ADD COLLABORATORS BUTTON--------------------------------------------------
+	    //ADD COLLABORATORS BUTTON
 		JButton addCollaboratorsButton = new JButton("Invite collaborators");
 		add(addCollaboratorsButton);
 	    addCollaboratorsButton.addActionListener(new ActionListener(){
@@ -133,32 +63,22 @@ public class ActivityModifierPanel extends JPanel {
 						e.printStackTrace();
 					}											
 					ArrayList<JCheckBox> checkboxes = new ArrayList<>();
-					if(!collaborators.isEmpty()){
-						for(int i = 0; i < collaborators.size(); i++) {
-							JCheckBox box = new JCheckBox(collaborators.get(i).getUsername());
-							checkboxes.add(box);
-							inviteFriends.add(box);
-						}
-					}
-					else{
-						JLabel noCollab = new JLabel("There are no collaborators in this project yet");
-						inviteFriends.add(noCollab);
-					}
+					for(int i = 0; i < collaborators.size(); i++) {
+						JCheckBox box = new JCheckBox(collaborators.get(i).getUsername());
+						checkboxes.add(box);
+						inviteFriends.add(box);
+					}				
 					JOptionPane invitingFriends = new JOptionPane();
 					int userId = 0;
 					int result2 = invitingFriends.showConfirmDialog(parentPanel, inviteFriends, "Invite your friends",JOptionPane.OK_CANCEL_OPTION); 
 					if (result2 == invitingFriends.OK_OPTION){
-						
 						try {
-							if(!checkboxes.isEmpty()){
-								for(int i = 0; i < checkboxes.size(); i++){
-									if(checkboxes.get(i).isSelected()){
-										userId = collaborators.get(i).getUserId();														
-									}	
+							for(int i = 0; i < checkboxes.size(); i++){
+								if(checkboxes.get(i).isSelected()){
+									userId = collaborators.get(i).getUserId();														
 								}
-								Client.getInstance().addAgent(activity.getActivityId(), userId);
-							}													
-							
+							}
+							Client.getInstance().addAgent(activity.getActivityId(), userId);
 							Window w = SwingUtilities.getWindowAncestor(thisPanel);	//codice per nascondere 
 							w.setVisible(false);									//la vecchia finestra							
 							JOptionPane newactivityPane = new JOptionPane();
