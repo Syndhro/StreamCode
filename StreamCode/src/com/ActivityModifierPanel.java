e4f1620188fb78035daba8ca847da4ff09a7753b
andreavaghi93 <andreavaghi93@yahoo.it>
2015-11-23 10:31:39 +0100

    Revert "Collaborators anche delle activity"
    This reverts commit 498b568e54c0ae05a3065f35e6fc7683d01f59df.

--- StreamCode/src/com/ActivityModifierPanel.java
+++ StreamCode/src/com/ActivityModifierPanel.java
@@ -1,24 +1,14 @@
 package com;
 
-import java.awt.Color;
-import java.awt.Window;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.rmi.RemoteException;
-import java.util.ArrayList;
 
-import javax.swing.BoxLayout;
-import javax.swing.JButton;
-import javax.swing.JCheckBox;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
-import javax.swing.SwingUtilities;
 
 public class ActivityModifierPanel extends JPanel {
 	
 	ActivityModifierPanel thisPanel;
 	ActivityPanel parentPanel;
-	Activity activity;
 	
 	public ActivityModifierPanel(int projectIndex, int activityIndex, ActivityPanel parentPanel) {
 		
@@ -26,70 +16,12 @@ public class ActivityModifierPanel extends JPanel {
 		this.parentPanel = parentPanel;
 		//retrieve activity
 		try {
-			activity = Client.getInstance().getManagedProject().get(projectIndex).getActivities().get(activityIndex);
+			Activity activity = Client.getInstance().getManagedProject().get(projectIndex).getActivities().get(activityIndex);
 		} catch (RemoteException e) {
 			e.printStackTrace();
 		}
 	    setBounds(100, 100, 500, 336);
-	    
-	    //ACTIVITY COLLABORATORS LIST
-	    JPanel collaboratorsPanel = new JPanel();
-		collaboratorsPanel. setLayout(new BoxLayout(collaboratorsPanel, BoxLayout.PAGE_AXIS));
-		ArrayList<User> collaborators = new ArrayList<User>();
-		ArrayList<JButton> collaboratorsButtons = new ArrayList<JButton>();
-		collaborators = activity.getActivityCollaborators();
-		for(int j = 0; j < collaborators.size(); j++){
-			if(!collaborators.isEmpty()){
-			JButton button = new JButton();
-			button.setText(collaborators.get(j).getUsername());
-			button.setBackground(Color.CYAN);
-			collaboratorsPanel.add(button);		
-			collaboratorsButtons.add(button);
-			}
-		}
-		add(collaboratorsPanel);
-	    
-	    //ADD COLLABORATORS BUTTON
-		JButton addCollaboratorsButton = new JButton("Invite collaborators");
-		add(addCollaboratorsButton);
-	    addCollaboratorsButton.addActionListener(new ActionListener(){
-			@Override
-			public void actionPerformed(ActionEvent arg0) {	
-				JPanel inviteFriends = new JPanel();						
-				ArrayList<User> collaborators = new ArrayList<User>();
-					try {
-						collaborators = Client.getInstance().getManagedProject().get(projectIndex).getCollaborators();
-					}catch(RemoteException e) {
-						e.printStackTrace();
-					}											
-					ArrayList<JCheckBox> checkboxes = new ArrayList<>();
-					for(int i = 0; i < collaborators.size(); i++) {
-						JCheckBox box = new JCheckBox(collaborators.get(i).getUsername());
-						checkboxes.add(box);
-						inviteFriends.add(box);
-					}				
-					JOptionPane invitingFriends = new JOptionPane();
-					int userId = 0;
-					int result2 = invitingFriends.showConfirmDialog(parentPanel, inviteFriends, "Invite your friends",JOptionPane.OK_CANCEL_OPTION); 
-					if (result2 == invitingFriends.OK_OPTION){
-						try {
-							for(int i = 0; i < checkboxes.size(); i++){
-								if(checkboxes.get(i).isSelected()){
-									userId = collaborators.get(i).getUserId();														
-								}
-							}
-							Client.getInstance().addAgent(activity.getActivityId(), userId);
-							Window w = SwingUtilities.getWindowAncestor(thisPanel);	//codice per nascondere 
-							w.setVisible(false);									//la vecchia finestra							
-							JOptionPane newactivityPane = new JOptionPane();
-							ActivityModifierPanel newactivityPanel = new ActivityModifierPanel(projectIndex, activityIndex, parentPanel);
-							newactivityPane.showConfirmDialog(parentPanel, newactivityPanel,"Activities", JOptionPane.CLOSED_OPTION);
-						}catch(RemoteException e1) {
-							e1.printStackTrace();
-						}	
-					}
-			}
-		});
+	  
 	}
 
 }
